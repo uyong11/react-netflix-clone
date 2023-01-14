f2a9a510dad1a50aaa15c7ab020d32fceb8df1e0
uyong\uyong <uyong11@daum.net>
2023-01-14 20:44:22 +0900

    이미지 배너 생성하기

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,7 +4,7 @@ const instance =axios.create({
     baseURL: "https://api.themoviedb.org/3",
     params: {
         api_key: "c9369cc1863607607840c2c192eba5ba",
-        languqge: "ko-KR",
+        language: "ko-KR",
     },
 });
 
