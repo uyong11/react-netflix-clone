8e76ce51a9b263c7cb309fb8d58ddbff79ab567a
uyong\uyong <uyong11@daum.net>
2023-01-15 17:40:03 +0900

    영화 나열을 위한 Row 컴포넌트 생성하기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import './App.css';
-import Nav from './components/Nav'
-import Banner from './components/Banner';
+import "./App.css";
+import Nav from "./components/Nav";
+import Banner from "./components/Banner";
+import requests from "./api/request";
+import Row from "./components/Row";
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        id="NO"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
+      <Row
+        title="Action Movies"
+        id="AN"
+        fetchUrl={requests.fetchActionMovies}
+      />
+      <Row
+        title="Comedy Movies"
+        id="CM"
+        fetchUrl={requests.fetchComedyMovies}
+      />
     </div>
   );
 }
